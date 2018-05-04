6f7def7615c764d9fc969fd65e8eb832d1872b5c
Esq Atom Smashers <ezekielc0535@sd61learn.ca>
2018-05-04 16:22:09 -0700

    Began building code for demo mode

--- src/org/usfirst/frc/team7287/robot/Robot.java
+++ src/org/usfirst/frc/team7287/robot/Robot.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 * @author  Esquimalt Robotics
 * @since   2018-02-01 
 */
+
 public class Robot extends IterativeRobot {
 	private DifferentialDrive spike;
 	private Joystick stick;
@@ -37,8 +38,8 @@ public class Robot extends IterativeRobot {
 	String switchAndScaleSides;
 	String closeSwitchSide;
 	int autoState;
-	DigitalInput startRight;
-	DigitalInput startLeft;
+	DigitalInput trustedDemo;
+	DigitalInput untrustedDemo;
 	
 	@Override
 	public void robotInit() {
@@ -50,11 +51,13 @@ public class Robot extends IterativeRobot {
 		clawHeightSensor = new ClawHeightSensor(0);
 		clawMotor = new TalonSRX(0);
 		verticalMotor = new TalonSRX(1);
-		startRight = new DigitalInput (0);
-		startLeft = new DigitalInput(1);
+		trustedDemo = new DigitalInput (0);
+		untrustedDemo = new DigitalInput(1);
 		topLimit = new DigitalInput (2);
 		bottomLimit = new DigitalInput(3);
 	}
+	//10 is left 00 is middle 01 is right
+	
 	
 
 	@Override
@@ -67,79 +70,81 @@ public class Robot extends IterativeRobot {
 	}
 	
 	
-	@Override
-	public void autonomousPeriodic() {
-		switchAndScaleSides = DriverStation.getInstance().getGameSpecificMessage();
-		this.closeSwitchSide = String.valueOf(switchAndScaleSides.charAt(0));
-		double turnTime = 0.5;
-		switch (autoState) {
-				case 0:
-					if (startLeft.get() || startRight.get()) {
-						//Robot is not in Middle
-						autoState = 4;
-						break;
-					}
-					if (closeSwitchSide.equals("L")) {
-						autoState = 2;
-					}
-					else {
-						autoState = 1;
-					}
-					break;
-				case 1:
-					//Drives straight into switch to deliver cube(middle right)
-					drive.forward(1.0);
-					if (timer.get() >= 1) {
-						timer.reset();
-						autoState = 10 ;
-					}
-					break;
-				case 2: //first part of s
-					double rightSideSpeed = 1;
-					drive.tankDrive(rightSideSpeed*0.45, rightSideSpeed);
-					if (timer.get() >= 0.8) {
-						timer.reset();
-						autoState = 3 ;
+//	@Override
+//	public void autonomousPeriodic() {
+//		switchAndScaleSides = DriverStation.getInstance().getGameSpecificMessage();
+//		this.closeSwitchSide = String.valueOf(switchAndScaleSides.charAt(0));
+//		double turnTime = 0.5;
+//		switch (autoState) {
+//				case 0:
+//					if (startLeft.get() || startRight.get()) {
+//						//Robot is not in Middle
+//						autoState = 4;
+//						break;
+//					}
+//					if (closeSwitchSide.equals("L")) {
+//						autoState = 2;
+//					}
+//					else {
+//						autoState = 1;
+//					}
+//					break;
+//				case 1:
+//					//Drives straight into switch to deliver cube(middle right)
+//					drive.forward(1.0);
+//					if (timer.get() >= 1) {
+//						timer.reset();
 //						autoState = 10 ;
-					}
-					break;
-				case 3: // drive forward to align with left side of switch
-					double leftSideSpeed = 1;
-					drive.tankDrive(leftSideSpeed,  leftSideSpeed*0.55);
-					if (timer.get() >= 0.86) {
-						timer.reset();
-						autoState = 11 ;
-					}
-					break;
-				case 4: //Left and Right Autonomous
-					drive.forward(0.5);
-					if (timer.get() <= 2.0) {
-						break;
-					}
-					
-					if (closeSwitchSide.equals("R") && startRight.get() && timer.get() > 2.0 && timer.get() < 3.0) {
-						drive.tankDrive(0.2, 0.6);
-					} else if (closeSwitchSide.equals("L") && startLeft.get() && timer.get() > 2.0 && timer.get() < 3.0) {
-						drive.tankDrive(0.6, 0.2);
-					} else {
-						timer.reset();
-						autoState = 10;
-					}
-					break;
-				case 10: // Hard Stop
-					drive.stop();
-					break;
-					
-				case 11: // Soft Stop
-					break;
-				}
-				
-	}
+//					}
+//					break;
+//				case 2: //first part of s
+//					double rightSideSpeed = 1;
+//					drive.tankDrive(rightSideSpeed*0.45, rightSideSpeed);
+//					if (timer.get() >= 0.8) {
+//						timer.reset();
+//						autoState = 3 ;
+////						autoState = 10 ;
+//					}
+//					break;
+//				case 3: // drive forward to align with left side of switch
+//					double leftSideSpeed = 1;
+//					drive.tankDrive(leftSideSpeed,  leftSideSpeed*0.55);
+//					if (timer.get() >= 0.86) {
+//						timer.reset();
+//						autoState = 11 ;
+//					}
+//					break;
+//				case 4: //Left and Right Autonomous
+//					drive.forward(0.5);
+//					if (timer.get() <= 2.0) {
+//						break;
+//					}
+//					
+//					if (closeSwitchSide.equals("R") && startRight.get() && timer.get() > 2.0 && timer.get() < 3.0) {
+//						drive.tankDrive(0.2, 0.6);
+//					} else if (closeSwitchSide.equals("L") && startLeft.get() && timer.get() > 2.0 && timer.get() < 3.0) {
+//						drive.tankDrive(0.6, 0.2);
+//					} else {
+//						timer.reset();
+//						autoState = 10;
+//					}
+//					break;
+//				case 10: // Hard Stop
+//					drive.stop();
+//					break;
+//					
+//				case 11: // Soft Stop
+//					break;
+//				}
+//				
+//	}
 
 	
 	@Override
 	public void teleopPeriodic() {
-		System.out.println("Switch is: Left: " + startLeft.get() + " Right: " + startRight.get());
+		if (trustedDemo == true or untrustedDemo == true) {
+			
+		}
 		double teleopSpeed = 1;
 		if (gantryController.getRawAxis(1) != 0) {
 			grab(gantryController.getRawAxis(1));
