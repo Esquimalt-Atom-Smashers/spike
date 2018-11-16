f82dd3a0d2b62a237b682a2d91cf52b6205a890c
Esq Atom Smashers <ezekielc0535@sd61learn.ca>
2018-11-16 12:56:20 -0800

    safemode

--- src/org/usfirst/frc/team7287/robot/Robot.java
+++ src/org/usfirst/frc/team7287/robot/Robot.java
@@ -140,7 +140,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		System.out.println("Switch is: Left: " + startLeft.get() + " Right: " + startRight.get());
-		double teleopSpeed = 1;
+		double teleopSpeed = 0.6;
 		if (gantryController.getRawAxis(1) != 0) {
 			grab(gantryController.getRawAxis(1));
 		}
