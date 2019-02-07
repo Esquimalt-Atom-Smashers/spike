01221d9e26a496ad8de0adea41e32498b5f18a04
Lucas Henry <lucasleroyhenry@gmail.com>
2019-02-06 21:02:13 -0800

    Spike cleaned up. R IP

--- src/org/usfirst/frc/team7287/robot/ClawControl.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team7287.robot;
-import com.ctre.CANTalon;
-
-public class ClawControl {
-	ClawControl(){
-		
-	}
-}
--- src/org/usfirst/frc/team7287/robot/Robot.java
+++ src/org/usfirst/frc/team7287/robot/Robot.java
@@ -1,4 +1,5 @@
 
+
 package org.usfirst.frc.team7287.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -6,13 +7,15 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import org.usfirst.frc.team7287.robot.Drive;
-import org.usfirst.frc.team7287.robot.ClawHeightSensor;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.cscore.*;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DriverStation;
+
+
 
 
 /**
@@ -21,184 +24,63 @@ import edu.wpi.first.wpilibj.DriverStation;
 * @author  Esquimalt Robotics
 * @since   2018-02-01 
 */
+
+// This is the 2018 robot. It had claws that moved in and out to grab boxes and the claws
+// were attached to a gantry to move them up and down as well. If you want to build this,
+// make sure to put the limit switches in for the gantry. Before we had that, the motor
+// would start smoking if you drove it too hard.
+// This is very bare-bones. There was more code, but it didn't really do anything so I
+// took it all out. It still works though, if you were to actually put it onto Spike. RIP
+// -Lucas Henry, programming head 2019
+
 public class Robot extends IterativeRobot {
 	private DifferentialDrive spike;
 	private Joystick stick;
 	private Joystick gantryController;
-	Timer timer;
-	int timeFactor;
-	int timeFix;
-	private Drive drive;	
-	ClawHeightSensor clawHeightSensor;
 	TalonSRX clawMotor;
 	TalonSRX verticalMotor;
 	DigitalInput topLimit;
 	DigitalInput bottomLimit;
-	String switchAndScaleSides;
-	String closeSwitchSide;
-	int autoState;
-	DigitalInput startRight;
-	DigitalInput startLeft;
+	
+	
+	
 	
 	@Override
 	public void robotInit() {
-		spike = new DifferentialDrive(new Spark(0), new Spark(1));
-		stick = new Joystick(0);
-		gantryController = new Joystick(1);
-		timer = new Timer();
-		drive = new Drive(spike, false);
-		clawHeightSensor = new ClawHeightSensor(0);
-		clawMotor = new TalonSRX(0);
-		verticalMotor = new TalonSRX(1);
-		startRight = new DigitalInput (0);
-		startLeft = new DigitalInput(1);
-		topLimit = new DigitalInput (2);
-		bottomLimit = new DigitalInput(3);
+		spike = new DifferentialDrive(new Spark(0), new Spark(1)); // the base and wheels
+		stick = new Joystick(0); // controls spike
+		gantryController = new Joystick(1); // controls arms
+		clawMotor = new TalonSRX(0); // talonSRX is name of motor driver
+		verticalMotor = new TalonSRX(1); // "
+		topLimit = new DigitalInput(0); // limit switch pressed by gantry moving up
+		bottomLimit = new DigitalInput(1); // same on bottom
 	}
 	
-
-	@Override
-	public void autonomousInit() {
-		timeFactor = 1;
-		timeFix = 0;
-		timer.reset();
-		timer.start();
-		autoState = 0;
-	}
-	
-	
-	@Override
-	public void autonomousPeriodic() {
-		switchAndScaleSides = DriverStation.getInstance().getGameSpecificMessage();
-		this.closeSwitchSide = String.valueOf(switchAndScaleSides.charAt(0));
-		double turnTime = 0.5;
-		switch (autoState) {
-				case 0:
-					if (startLeft.get() || startRight.get()) {
-						//Robot is not in Middle
-						autoState = 4;
-						break;
-					}
-					if (closeSwitchSide.equals("L")) {
-						autoState = 2;
-					}
-					else {
-						autoState = 1;
-					}
-					break;
-				case 1:
-					//Drives straight into switch to deliver cube(middle right)
-					drive.forward(1.0);
-					if (timer.get() >= 1) {
-						timer.reset();
-						autoState = 10 ;
-					}
-					break;
-				case 2: //first part of s
-					double rightSideSpeed = 1;
-					drive.tankDrive(rightSideSpeed*0.45, rightSideSpeed);
-					if (timer.get() >= 0.8) {
-						timer.reset();
-						autoState = 3 ;
-//						autoState = 10 ;
-					}
-					break;
-				case 3: // drive forward to align with left side of switch
-					double leftSideSpeed = 1;
-					drive.tankDrive(leftSideSpeed,  leftSideSpeed*0.55);
-					if (timer.get() >= 0.86) {
-						timer.reset();
-						autoState = 11 ;
-					}
-					break;
-				case 4: //Left and Right Autonomous
-					drive.forward(0.5);
-					if (timer.get() <= 2.0) {
-						break;
-					}
-					
-					if (closeSwitchSide.equals("R") && startRight.get() && timer.get() > 2.0 && timer.get() < 3.0) {
-						drive.tankDrive(0.2, 0.6);
-					} else if (closeSwitchSide.equals("L") && startLeft.get() && timer.get() > 2.0 && timer.get() < 3.0) {
-						drive.tankDrive(0.6, 0.2);
-					} else {
-						timer.reset();
-						autoState = 10;
-					}
-					break;
-				case 10: // Hard Stop
-					drive.stop();
-					break;
-					
-				case 11: // Soft Stop
-					break;
-				}
-				
-	}
-
 	
 	@Override
 	public void teleopPeriodic() {
-		System.out.println("Switch is: Left: " + startLeft.get() + " Right: " + startRight.get());
-		double teleopSpeed = 0.6;
-		if (gantryController.getRawAxis(1) != 0) {
-			grab(gantryController.getRawAxis(1));
-		}
-		else {
-			grab(0);
-		}
-
-		if (gantryController.getRawAxis(3) != 0){
-			
-			upDown(gantryController.getRawAxis(3));
-		}
-		else {
-			upDown(0);
-		}
-		
-		spike.arcadeDrive(stick.getY()*teleopSpeed, stick.getRawAxis(2)*teleopSpeed);
+		grab(gantryController.getRawAxis(1)); // claws
+		upDown(gantryController.getRawAxis(3)); // gantry up and down
+		spike.arcadeDrive(stick.getY(), stick.getRawAxis(2)); // move around
 	}
 	
 	
-	private void upDown(double move) {	
-		move = -move; //invert move directions
+	private void upDown(double move) {	// move gantry up or down
+		move *= -1; //invert move directions
 		
 		if (move > 0) {
-			move = move * 0.9;
+			move *= 0.9; // make sure it doesn't go too fast
 		}
-//		if (move< 0 && topLimit.get() || move>0 && bottomLimit.get()) {
-			if (move< 0 && topLimit.get()) {
+		if (move < 0 && topLimit.get() || move > 0 && bottomLimit.get()) { // stop motor burnout 
 			move = 0;
-			}
-			verticalMotor.set(ControlMode.PercentOutput, move);
-		}	
+			System.out.println("Limit switch pressed");
+		}
+		verticalMotor.set(ControlMode.PercentOutput, move);
+	}	
 	
 	private void grab(double speed) {
 		clawMotor.set(ControlMode.PercentOutput, speed * 0.7);
 	}
-	
-	
-	private void drop() {
-		clawMotor.set(ControlMode.PercentOutput, 0);
-	}
-	
-
-	private void calibrate() {
-		double turnSpeed = 0.46;
-		double linearSpeed = 0.5;
-		if (timer.get() > timeFactor + timeFix - 1.0 && timer.get() < timeFactor + timeFix) {
-			drive.forward(linearSpeed);
-		} else if (timer.get() > timeFactor + timeFix && timer.get() < timeFactor + timeFix + 0.5) {
-			drive.turn("left",turnSpeed);
-		} else if (timer.get()> timeFactor + timeFix + 0.5 && timer.get() < timeFactor + timeFix + 1.0) {
-			drive.stop();
-		} else {
-			drive.stop();
-			timeFix++;
-			timeFactor++;
-		}
-	}
-	
 	
 }// end of Robot Class
 
